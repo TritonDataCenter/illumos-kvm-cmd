6cfe9254bc760ee5b8edcc35b6f767f00aabcb6f
Josh Wilsdon <josh@wilsdon.ca>
2011-08-02 20:59:32 +0000

    [HVM-489] build kvm-cmd with png support for VNC.

--- build.sh
+++ build.sh
@@ -7,8 +7,32 @@ for dir in seabios vgabios kvm/test; do
     cp roms/${dir}/config.mak.tmpl roms/${dir}/config.mak
 done
 
+PNGDIR="${PWD}/libpng-1.5.4"
+PNGINC="${PNGDIR}/proto/usr/local/include"
+PNGLIB="${PNGDIR}/proto/usr/local/lib"
+
+if [[ ! -d ${PNGDIR} ]]; then
+    (curl ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.4.tar.gz | \
+        tar -zxf -)
+    if [[ $? != "0" || ! -d ${PNGDIR} ]]; then
+        echo "Failed to get libpng."
+        rm -rf ${PNGDIR}
+        exit 1
+    fi
+fi
+
+if [[ ! -e ${PNGLIB}/libpng.a ]]; then
+    (cd ${PNGDIR} && \
+        LDFLAGS=-m64 CFLAGS=-m64 ./configure --disable-shared && \
+        make && \
+        mkdir -p ${PNGDIR}/proto && \
+        make DESTDIR=${PNGDIR}/proto install)
+fi
+
 echo "==> Running configure"
 ./configure \
+    --extra-cflags="-I${PNGDIR}/proto/usr/local/include" \
+    --extra-ldflags="-L${PNGDIR}/proto/usr/local/lib -lz -lm" \
     --prefix=/smartdc \
     --audio-card-list= \
     --audio-drv-list= \
@@ -18,9 +42,9 @@ echo "==> Running configure"
     --enable-debug \
     --enable-kvm \
     --enable-kvm-pit \
+    --enable-vnc-png \
     --disable-kvm-device-assignment \
     --disable-sdl \
-    --disable-vnc-png \
     --disable-vnc-jpeg \
     --disable-vnc-sasl \
     --disable-vnc-tls \
