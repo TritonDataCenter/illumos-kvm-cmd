1c6181be55d1cadc4426069960688307a6083131
Joshua M. Clulow <jmc@joyent.com>
2013-10-25 03:42:07 +0000

    HVM-785 kvm-cmd build should use CTFMERGE and CTFCONVERT

--- ctf.sh
+++ ctf.sh
@@ -5,12 +5,8 @@
 # ctfconvert is run with the -i option, it will delete the .o file. This is bad.
 # Instead we end up using a temporary file and move over it. 
 #
-# This file gets invoked from inside the x86-64_softmmu directory, hence the
-# extra .. in the path below. That's kind of ugly, and I almost apologize.
-#
 
 sh_arg0=$(basename $0)
-ctf_bin=$(pwd)/../../../illumos/usr/src/tools/proto/root_i386-nd/opt/onbld/bin/i386/ctfconvert
 
 function fail
 {
@@ -20,11 +16,14 @@ function fail
         exit 1
 }
 
-
 [[ $# -eq 1 ]] || fail "missing arguments"
 
+# CTFCONVERT may contain a wildcard, so expand it out:
+ctfconvert=$(echo ${CTFCONVERT})
+[[ -x ${ctfconvert} ]] || fail "could not find ctfconvert at $CTFCONVERT"
+
 echo "Converting $1"
-$ctf_bin -L VERSION -o $1.ctf $1
+$ctfconvert -L VERSION -o $1.ctf $1
 [[ $? -ne 0 ]] && exit 1
 mv $1.ctf $1
 exit 0
